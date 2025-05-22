319a080c09afd97b090918be6bc454f3b046eca3
Mateusz Cegiełka <mateusz@cegla.net>
2025-05-22 19:50:55 +0200

    Remove interpreter

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,8 @@ use crate::ast::Module;
 
 pub mod ast;
 pub mod codegen;
-mod interpreter;
 mod parser;
 
 pub fn parse(source: &str) -> Module {
     parser::module(source)
-}
-
-pub fn interpret(module: &Module) -> String {
-    interpreter::run(module)
 }
