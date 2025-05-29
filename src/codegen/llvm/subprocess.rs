a186856037d74128a1d1d1d0bec0339d776d907f
Mateusz Cegiełka <mateusz@cegla.net>
2025-05-29 20:21:11 +0200

    Remove dependency on lld instead of ld

--- src/codegen/llvm/subprocess.rs
+++ src/codegen/llvm/subprocess.rs
@@ -4,31 +4,42 @@ use std::process::{Command, Stdio};
 
 pub fn compile_ir(ir: &str, output_path: Option<&Path>) -> Result<(), String> {
     let object_path = tempfile::NamedTempFile::new().unwrap().into_temp_path();
+    llc(ir, &object_path)?;
+    ld(&object_path, output_path)?;
+    Ok(())
+}
 
-    let mut llc_child = Command::new("llc")
+fn llc(ir: &str, object_path: &Path) -> Result<(), String> {
+    let mut child = Command::new("llc")
         .args(["--filetype", "obj", "-", "-o"])
-        .arg(&object_path)
+        .arg(object_path)
         .stdin(Stdio::piped())
         .stderr(Stdio::piped())
         .spawn()
         .unwrap();
 
-    let mut llc_stdin = llc_child.stdin.take().unwrap();
-    llc_stdin.write_all(ir.as_bytes()).unwrap();
-    drop(llc_stdin);
+    let mut stdin = child.stdin.take().unwrap();
+    stdin.write_all(ir.as_bytes()).unwrap();
+    drop(stdin);
 
-    let llc_output = llc_child.wait_with_output().unwrap();
-    if !llc_output.status.success() {
-        return Err(String::from_utf8(llc_output.stderr).unwrap());
+    let output = child.wait_with_output().unwrap();
+    if !output.status.success() {
+        return Err(String::from_utf8(output.stderr).unwrap());
     }
+    Ok(())
+}
 
-    let mut lld_command = Command::new("ld.lld");
-    lld_command.arg(&object_path);
+fn ld(object_path: &Path, output_path: Option<&Path>) -> Result<(), String> {
+    let mut command = Command::new("ld");
+    command.arg(object_path);
     if let Some(output_path) = output_path {
-        lld_command.arg("-o").arg(output_path);
+        command.arg("-o").arg(output_path);
     }
-    let lld_output = lld_command.output().unwrap();
-    assert!(lld_output.status.success());
+    command.stderr(Stdio::piped());
 
+    let output = command.output().unwrap();
+    if !output.status.success() {
+        return Err(String::from_utf8(output.stderr).unwrap());
+    }
     Ok(())
 }
